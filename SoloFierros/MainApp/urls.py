ffc53d113c7774b47ed6b8d25d2e37435ab55c52
Carlos Tolosa <carloswtolosa@gmail.com>
2022-12-01 21:41:42 -0300

    Retoques 1 - Tolosa

--- SoloFierros/MainApp/urls.py
+++ SoloFierros/MainApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from MainApp.views import *
 
 urlpatterns = [
-    path("inicio/", vista_inicio)
+    path("", vista_inicio, name="inicio"),
+    path("about/", vista_nosotros, name="nosotros"),
+    path("busqueda/autos/resultados/", busqueda_autos_resultados, name="busqueda-autos-resultados"),
+
 ]
--- SoloFierros/MainApp/views.py
+++ SoloFierros/MainApp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from MainApp.models import *
 
 # Create your views here.
 
 def vista_inicio(request):
     return render(request, "MainApp/index.html")
+
+def vista_nosotros(request):
+    return render(request, "MainApp/about.html")
+
+def busqueda_autos_resultados(request):
+
+    nombre_automovil = request.GET["nombre_automovil"]
+    automovil = Automovil.objects.filter(nombre__icontains=nombre_automovil)
+
+    return render(request, "MainApp/busqueda_autos_resultados.html", {"automovil": automovil})
